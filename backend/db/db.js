3e828411a7a69506f64aa7b2b6f23b3dfc1ae703
aaryan <aaryaniiitu@gmail.com>
2026-02-07 17:11:54 +0530

    add database logic

---
+++ backend/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+    }
+    catch (err) {
+        console.error("MongoDB connection error:", err.message);
+        process.exit(1);    //Exit the Node.js process with a failure code (1).
+    }
+}
+
+
+export default connect;
